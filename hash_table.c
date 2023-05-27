789cefcb0b8720bfb0e85f16015576a732d5c0e3
Luis Roberto <roberto4luis1@gmail.com>
2023-05-27 16:45:11 -0300

    setting files

---
+++ hash_table.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+to do list:
+    hash function;
+    hash inserting
+    hash treatment
+    hash search
+
+*/
+
+struct node
+{
+    void *data;
+    struct node *next;
+};
+
+
+
+int hash
