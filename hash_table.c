ee7e8fb90d389b90d55dd06e8b4ebede5a61926f
Luis Roberto <roberto4luis1@gmail.com>
2023-05-27 18:53:55 -0300

    part of the hash table / license, gitgnore files

--- hash_table.c
+++ hash_table.c
@@ -1,5 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
+/*
+    I'm going to use chaing for the colision treatment,
+    and make a more generic hash table using void*.
+*/
 
 /*
 to do list:
@@ -10,12 +12,65 @@ to do list:
 
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#define sizeh 10
+
+// struct for storing each data and implementing chaining
 struct node
 {
-    void *data;
+    int data;
     struct node *next;
-};
 
+} typedef node;
 
+struct hash_table
+{
+    node *array[sizeh];
+    int quantity
 
-int hash
+} typedef hash_table;
+
+// This hash function is used only to give a example, It's generic and is going to cause a lot of colision
+void hashinicialize(hash_table **hash)
+{
+    for (int i = 0; i < sizeh; i++)
+        (*hash)->array[i] = NULL;
+    (*hash)->quantity = 0;
+}
+
+int hashfunction(int object)
+{
+
+    int value = object % sizeh;
+
+    return value;
+}
+
+node *allocnode(int object)
+{
+    node *item = (node *)malloc(sizeof(node));
+
+    item->data = object;
+    item->next = NULL;
+    return item;
+}
+
+void insert_hash(hash_table **hash_t, int object)
+{
+    int key = hashfunction(object);
+    node *item = allocnode(object);
+
+    if ((*hash_t)->array[key] = NULL)
+    {
+        (*hash_t)->array[key] = item;
+    }
+    else
+    {
+        node *aux = (*hash_t)->array[key];
+        while (aux->next != NULL)
+            aux = aux->next;
+        aux->next = item;
+    }
+    (*hash_t)->quantity++;
+}
--- main.c
+++ main.c
@@ -4,9 +4,13 @@
 
 int main(int argc, char *argv){
 
-    
+    char a = 'B';
+    void *p = &a;
+    printf("%d, %d", *(char *)p, *(char *)p);
 
 
+    int num = hashfunction(p);
+    printf(" %d", num);
 
     return 0;
 }
